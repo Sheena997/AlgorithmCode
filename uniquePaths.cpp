4ae2198c704ed4f290db8717db3a7caaadc9c70c
Sheena爽 <18365368008@163.com>
2020-01-12 21:27:16 +0800

    施爽：于2020年1月12日上传的动态规划机器人有多少种路径从起点到终点的代码。

---
+++ uniquePaths.cpp
@@ -0,0 +1,25 @@
+/*
+动态规划
+链接：https://www.nowcoder.com/questionTerminal/166eaff8439d4cd898e3ba933fbc6358
+来源：牛客网
+一个机器人在m×n大小的地图的左上角（起点，下图中的标记“start"的位置）。
+机器人每次向下或向右移动。机器人要到达地图的右下角。（终点，下图中的标记“Finish"的位置）。
+可以有多少种不同的路径从起点走到终点？
+上图是3×7大小的地图，有多少不同的路径？
+备注：m和n小于等于100
+*/
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for(int i = 1; i < m; ++i)
+        {
+            for(int j = 1; j < n; ++j)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
