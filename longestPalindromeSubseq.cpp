c8b0fc1fe640d6550ea86e6035490f44014ec1eb
Sheena爽 <18365368008@163.com>
2020-06-30 00:32:27 +0800

    施爽：于2020年6月30日上传的求最长回文子序列长度（dp）的代码。

--- longestPalindromeSubseq.cpp
+++ longestPalindromeSubseq.cpp
@@ -29,16 +29,19 @@ public:
     int longestPalindromeSubseq(string s) {
         int n = s.size();
         vector<vector<int>> dp(n, vector<int>(n, 0));
+        dp[0][0] = 1;
         
-        for(int i = n - 1; i >= 0; --i)
-        {
+        for(int i = 0; i < n; ++i)
             dp[i][i] = 1;
-            for(int j = i + 1; j < n; ++j)
+            
+        for(int i = 1; i < n; ++i)
+        {
+            for(int j = 0; j < n - i; ++j)
             {
-                if(s[i] == s[j])
-                    dp[i][j] = 1 + dp[i + 1][j - 1] + 1;
+                if(s[j] == s[j + i])
+                    dp[j][j + i] = 1 + dp[j + 1][i + j - 1] + 1;
                 else
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                    dp[j][j + i] = max(dp[j + 1][j + i], dp[j][i + j - 1]);
             }
         }
         
