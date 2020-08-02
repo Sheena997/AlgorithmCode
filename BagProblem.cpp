9e375ff19b6f5e458ecd77ead61c131cb538a488
Sheena爽 <18365368008@163.com>
2020-08-02 23:05:50 +0800

    施爽：于2020年8月2日上传的背包问题dp（01背包、完全背包、多重背包）的代码。

--- BagProblem.cpp
+++ BagProblem.cpp
@@ -33,20 +33,17 @@ int BagProblem(int n, int m, vector<int> w, vector<int> v)
 	if(v.empty() || w.empty() || m < 1)
         return 0;
 
-	vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+	vector<int> dp(m + 1, 0);
 
 	for(int i = 1; i < n + 1; ++i)
     {
-        for(int j = 1; j < m + 1; ++j)
+        for(int j = m; j >= w[i - 1]; --j)
         {
-            if(w[i - 1] > j)
-                dp[i][j] = dp[i - 1][j];
-            else
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i - 1]] + v[i - 1]);
+			dp[j] = max(dp[j], dp[j - w[i - 1]] + v[i - ]);
         }
     }
 
-	return dp[n][m];
+	return dp[m];
 }
 int main()
 {
@@ -64,4 +61,78 @@ int main()
 		cout << res << endl;
 
 	return 0;
+}
+
+
+//完全背包
+/*
+有编号分别为a,b,c,d的四件物品，它们的重量分别是2,3,4,7，
+它们的价值分别是1,3,5,9，每件物品数量无限个，现在给你个
+承重为10的背包，如何让背包里装入的物品具有最大的价值总和？
+
+完全背包问题与01背包问题的区别在于每一件物品的数量都有无限个，
+而01背包每件物品数量只有一个。
+*/
+int BagProblem(int n, int m, vector<int> w, vector<int> v)
+{
+	if(v.empty() || w.empty() || m < 1)
+        return 0;
+
+	vector<int> dp(m + 1, 0);
+	for(int i = 1; i < n + 1; ++i)
+	{
+		for(int j = w[i - 1]; j < m + 1; ++j)
+			dp[j] = max(dp[j], dp[j - w[i - 1]] + v[i - 1]);
+	}
+	return dp[m];
+}
+int main()
+{
+	int n = 0, m = 0;
+	cin >> n >> m;
+		vector<int> w(n, 0);
+		vector<int> v(n, 0);
+		for (auto& e : w)
+			cin >> e;
+		for (auto& e : v)
+			cin >> e;
+
+		int res = BagProblem(n, m, w, v);
+
+		cout << res << endl;
+
+	return 0;
+}
+
+
+
+
+
+//多重背包：
+/*
+有编号分别为a,b,c的三件物品，它们的重量分别是1，2，2，它们
+的价值分别是6，10，20，他们的数目分别是10，5，2，现在给你个
+承重为 8 的背包，如何让背包里装入的物品具有最大的价值总和？
+
+有 n 种物品和一个承重是 m 的背包。第 i 种物品最多有 si 件，每件重量是 wi，价值是 vi。
+求解将哪些物品装入背包，可使物品重量总和不超过背包容量，且价值总和最大。输出最大价值。
+
+多重背包和01背包、完全背包的区别：多重背包中每个物品的个数都
+是给定的，可能不是一个，绝对不是无限个。
+*/
+int BagProblem(int n, int m, vector<int> w, vector<int> v, vector<int> s)
+{
+	if(v.empty() || w.empty() || m < 1)
+        return 0;
+
+	vector<int> dp(m + 1, 0);
+	for(int i = 1; i < n + 1; ++i)
+	{
+		for(int j = m; j >= 0; --j)
+		{
+			for(int k = 1; k <= s[i - 1] && k * w[i - 1] <= j; ++k)
+				fp[j] = max(dp[j], dp[j - k * w[i - 1]] + k * v[i - 1]);
+		}
+	}
+	return dp[m];
 }
